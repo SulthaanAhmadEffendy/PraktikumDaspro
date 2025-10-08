d74bfacf1c8f156ac37159bf1f6a9c7d40919857
SulthaanAhmadEffendy <sulthaneffendy@gmail.com>
2025-10-08 08:58:45 +0700

    Percobaan 3

--- JobSheet05/PemilihanHariIf.java
+++ JobSheet05/PemilihanHariIf.java
@@ -1,6 +1,6 @@
 package JobSheet05;
 import java.util.Scanner;
-public class PemilihanHariDenganIf24 {
+public class PemilihanHariIf {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
---
+++ JobSheet05/UjianSkripsi.java
@@ -0,0 +1,33 @@
+package JobSheet05;
+
+import java.util.Scanner;
+public class UjianSkripsi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String pesan;
+        System.out.print("Apakah mahasiswa sudah bebas kompen? (Ya/Tidak: ");
+        String bebasKompen = sc.nextLine().trim();
+        System.out.print("Masukkan jumlah log bimbingan Pembimbing 1:");
+        int bimbinganP1 = sc.nextInt();
+        System.out.print("Masukkan jumlah log bimbingan Pembimbing 2:");
+        int bimbinganP2 = sc.nextInt();
+
+        if (bebasKompen.equalsIgnoreCase("Ya")){
+            if (bimbinganP1 >= 8 && bimbinganP2 >= 4){
+                pesan = "Semua Syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi";
+            } else if (bimbinganP1 < 8 && bimbinganP2 < 4) {
+                pesan = "Gagal, log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali";
+            } else if (bimbinganP1 < 8) {
+                pesan = "Gagal, log bimbingan P1 kurang dari 8 kali";
+            } else {
+                pesan = "Gagal, log bimbingan P2 kurang dari 4 kali";
+            }
+        } else {
+            pesan = "Gagal, mahasiswa belum bebas kompen";
+        }
+        System.out.println(pesan);
+
+    }
+
+
+}
