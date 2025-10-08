0834fb372b2f38ecb0daeae72709286b552d8ddc
SulthaanAhmadEffendy <sulthaneffendy@gmail.com>
2025-10-08 08:31:14 +0700

    Modifikasi Percobaan 1

--- JobSheet05/CetakKRS24.java
+++ JobSheet05/CetakKRS24.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 public class CetakKRS24 {
     public static void main(String[] args) {
 
-       
         Scanner sc = new Scanner(System.in);
         System.out.println("Cetak KRS SIAKAD");
         System.out.print("Apakah Ukt Anda Sudah Lunas? (true/false) : ");
         boolean uktLunas = sc.nextBoolean();
-        String hasil = (uktLunas == true) ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan dpa" : "Registrasi Ditolak. Silahkan lunasi UKT Anda";
-        System.out.println(hasil);
+        if (uktLunas == true) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan dpa");
+        } else {
+            System.out.println("Registrasi Ditolak. Silahkan lunasi UKT Anda");
+        }
     }
 }
